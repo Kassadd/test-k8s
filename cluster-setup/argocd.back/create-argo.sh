01bdf2d34d679b9d24cd869d78a44be15a7678c6
Javier Ruiz <javi@Javiers-iMac.local>
2020-01-02 01:28:31 +0100

    aws-node-magn

--- cluster-setup/argocd.back/create-argo.sh
+++
@@ -1,9 +0,0 @@
-kubectl apply -f argo-namespace.yaml -f install.yaml -n argocd
-sleep 10
-kubectl apply -f argo-secrets.yaml -f argo-configMap.yaml -f argo-argoApps.yaml -n argocd
-#sleep 10
-#kubectl delete --all pods -n argocd
-
-sleep 10
-echo "kubectl port-forward svc/argocd-server -n argocd 8080:443"
-
